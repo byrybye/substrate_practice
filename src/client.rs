e9e34932923b7aefa5729be9d5e73fe25d54dae0
byrybye <byrybye@hotmail.com>
2022-07-07 20:54:40 +0800

    增加注释

--- src/client.rs
+++ src/client.rs
@@ -1,35 +1,39 @@
-
-use std::str;
 use std::thread;
-use std::thread::JoinHandle;
 use std::time::Duration;
 use std::{
     io::{Read, Write},
-    net::{TcpListener, TcpStream},
+    net::TcpStream,
 };
 
-#[warn(unused_must_use)]
-#[warn(unused_imports)]
 fn main() {
     println!("客户端启动");
+    //客户端 连接服务端
     let client_result = TcpStream::connect::<String>("127.0.0.1:6666".into());
+    //对连接结果做匹配模式处理
     match client_result {
+        //如果连接成功
         Ok(mut client) => {
             println!("客户端连接成功");
+            //循环10次 消息
             for i in 0..10 {
                 println!("第{}次，开始发送", i + 1);
+                //构建消息内容
                 let message = format!("{}：hello world", i + 1);
                 println!("消息内容：{}", message);
-                client.write(message.as_bytes());
+                //发送消息
+                let _ = client.write(message.as_bytes());
                 println!("第{}次，发送完成", i + 1);
+                //暂停一秒 方便观察
                 thread::sleep(Duration::from_secs(1));
+                //声明数组用于读取数据
                 let mut buffer = [0; 1024];
+                //读取数据
                 client.read(&mut buffer).unwrap();
                 println!("从服务端收到数据: {}", String::from_utf8_lossy(&buffer[..]));
-                
             }
             println!("客户端发送任务完成");
         }
+        //如果发生错误，打印错误信息，退出程序
         Err(error) => {
             println!("{} {:#?}", "客户端连接时发生错误:", error);
             return;
--- src/server.rs
+++ src/server.rs
@@ -1,37 +1,48 @@
-#[warn(unused_must_use)]
-#[warn(unused_imports)]
-use std::str;
+
 use std::thread;
-use std::thread::JoinHandle;
 use std::{
     io::{Read, Write},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener},
 };
 
 fn main() {
     println!("服务端启动");
+    //绑定ip 和 端口 开始监听
     let server_result = TcpListener::bind::<String>("127.0.0.1:6666".into());
+    //如果发生错误
     if server_result.is_err() {
+        //获取错误信息并打印
         let error = server_result.err().unwrap();
         print!("{} {:#?}", "服务端开始监听时发生错误:", error);
+        //结束程序运行
         return;
     }
+    //获取 服务端监听器
     let server = server_result.unwrap();
     println!("服务端开始监听");
+    //建立 vec 容器存储 线程句柄
     let mut handler_vec: Vec<thread::JoinHandle<()>> = Vec::new();
 
+    //获取一个链接
     for stream in server.incoming() {
+        //用match 匹配处理
         match stream {
             Ok(mut stream) => {
                 println!("开始一次会话: {:#?}", stream);
+                //开启线程 处理接收的信息
                 let handler = thread::spawn(move || {                    
-                    loop {
+                    //使用循环处理一次连接 会话
+                    loop {                     
+                        //声明数组用来从stream里读取数据   
                         let mut buffer = [0; 1024];
+                        //将数据从stream读到buffer中
                         stream.read(&mut buffer).unwrap();
+                        //将buffer内容转成字符串
                         let messge = String::from_utf8_lossy(&buffer[..]);
                         println!("从客户端收到数据：{}", messge);
-                        stream.write(format!("echo: {}", messge).as_bytes());
-
+                        //向客户端发送回声消息
+                        let _ = stream.write(format!("echo: {}", messge).as_bytes());
+                        //如果消息开头是 10 那么 结束一次会话，跳出循环
                         if messge.starts_with("10") {
                             println!("结束一次会话: {:#?}", stream);
                             break;
@@ -40,13 +51,15 @@ fn main() {
                 });                
                 handler_vec.push(handler);                
             }
+            //如果发生错误 打印错误信息
             Err(error) => {
                 print!("{} {:#?}", "获取客户端数据发生错误:", error);
             }
         }
     }
+    //在主线程等待 子线程结束
     for handler in handler_vec {
         handler.join().unwrap();
     }    
-    println!("服务端监听任务结束");
+    //println!("服务端监听任务结束");
 }
