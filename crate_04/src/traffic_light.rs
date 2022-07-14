51fd0616b2591595d6317e3b07326e42670140e0
byrybye <byrybye@hotmail.com>
2022-07-14 21:05:59 +0800

    04作业

--- crate_04/src/traffic_light.rs
+++ crate_04/src/traffic_light.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+#[derive(Debug, Clone, Copy)]
+pub enum TrafficLight {
+    Red,
+    Green,
+    Yellow,
+}
+
+pub trait TrafficLightDuration {
+    fn get_duration(light: TrafficLight) -> Duration {
+        match light {
+            TrafficLight::Red => Duration::from_secs(5),
+            TrafficLight::Green => Duration::from_secs(10),
+            TrafficLight::Yellow => Duration::from_secs(3)            
+        }
+    }
+}
+
+pub struct  ShangHai
+{
+
+}
+impl TrafficLightDuration for ShangHai {
+    
+}
+
+fn main() {    
+    let g = TrafficLight::Green;        
+    let r = TrafficLight::Red;        
+    let y = TrafficLight::Yellow;        
+    println!("light: {:#?}  duration: {:#?}" , g, ShangHai::get_duration(g));
+    println!("light: {:#?}  duration: {:#?}" , r, ShangHai::get_duration(r));
+    println!("light: {:#?}  duration: {:#?}" , y, ShangHai::get_duration(y));
 }
--- crate_04/src/u32_vec.rs
+++ crate_04/src/u32_vec.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {        
+    let list1: [u32; 3] = [1, 2, 3];
+    let sum1 = sum(&list1);
+    println!("{:#?}", sum1.unwrap());
+
+    let list2: [u32; 2] = [1, u32::MAX];
+    let sum2 = sum(&list2);
+    println!("{:#?}", sum2.unwrap());
+}
+
+fn sum(list: &[u32]) -> Option<u32> {    
+    let mut total: u32 = 0;
+    for item in list {        
+        let (t, is_error) = total.overflowing_add(*item);          
+        if is_error {
+            return Some(0);
+        } else {
+            total = t;
+        }
+    }
+    return Some(total);
 }
